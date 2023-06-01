a0f640145ab6a8b873c8cf4102bb1df3928ee1ed
Pumblechook333 <sabastianf531@gmail.com>
2023-06-01 17:19:16 -0400

    filtered vs unfiltered binning

--- dshift_5min_dist.py
+++ dshift_5min_dist.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 from scipy.signal import filtfilt, butter
-from collections import Counter
 from math import floor
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -33,16 +32,16 @@ for line in lines[19::n]:
 # Filtering the data with order 3 Butterworth low-pass filter
 # Butterworth Order: https://rb.gy/l4pfm
 
-FILTERORDER = 3     # Order (Falloff Rate)
-FILTERBREAK = .005  # Critical Freq. (Freq to begin falloff)
-b, a = butter(FILTERORDER, FILTERBREAK, analog=False, btype='low')
-
-freq_filt = filtfilt(b, a, freq)
+# FILTERORDER = 3     # Order (Falloff Rate)
+# FILTERBREAK = .005  # Critical Freq. (Freq to begin falloff)
+# b, a = butter(FILTERORDER, FILTERBREAK, analog=False, btype='low')
+#
+# freq_filt = filtfilt(b, a, freq)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Converting data to correct units
 
-f_range = [(f-10e6) for f in freq_filt]                      # Doppler shifts (del from 10GHz)
+f_range = [(f-10e6) for f in freq]                      # Doppler shifts (del from 10GHz)
 N = len(f_range)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -55,36 +54,28 @@ while not index > N:
     f_subranges.append(f_range[index:index+N_ranges])
     index += N_ranges
 
+f_hour1 = f_subranges[0:12]     # 12 5-min sections of data (hour 1)
+
 index = 0
 
-for srange in f_subranges:
+for srange in f_hour1:
 
     print('Resolving subrange: ' + str(index) + ' ('
-          + str(floor((index / N) * 100)) + '% complete)')
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Counting individual occurrences
-
-    # f_counts = Counter(srange)
-    # f_keys = f_counts.keys()
-    # f_values = f_counts.values()
+          + str(floor((index / len(f_hour1)) * 100)) + '% complete)')
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Plot the subsections
+    # binlims = [i/10 for i in range(-10, 11, 1)]     # 0.1Hz Bins (-1Hz to +1Hz)
+    binlims = [i/10 for i in range(-25, 26, 1)]     # 0.1Hz Bins (-2.5Hz to +2.5Hz)
 
     fig = plt.figure(figsize=(19, 10))      # inches x, y with 72 dots per inch
     ax1 = fig.add_subplot(111)
-    ax1.hist(srange, color='r', bins=int(N_ranges/(5*6)))         # 10s long bins
+    ax1.hist(srange, color='r', bins=binlims)
     ax1.set_xlabel('Doppler Shift, Hz')
-    ax1.set_ylabel('Counts, N (10 sec bin)', color='r')
+    ax1.set_ylabel('Counts, N', color='r')
     # ax1.set_xlim([-1, 1])
-    # for tl in ax1.get_yticklabels():
-    #     tl.set_color('r')
-
-    # ax2 = ax1.twinx()
-    # ax2.scatter(f_keys, f_values, s=5, marker='*', color='k')
-    # ax2.set_ylabel('Counts, N', color='k')
-    # ax2.set_ylim(0, 3)                   # -1 to 1 Hz for Doppler shift
+    ax1.set_xlim([-2.5, 2.5])
+    ax1.set_xticks(binlims[::2])
 
     plt.title('WWV 10 MHz Doppler Shift Distribution Plot ' + str(index) + ' \n'                   # Title (top)
               'Node: N0000020    Gridsquare: FN20vr \n'
@@ -96,4 +87,3 @@ for srange in f_subranges:
     plt.close()
 
     index += 1
-
